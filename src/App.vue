2da62196dfcd685a103ba2ffa0422eb3ad1983f8
Theautz <waritxapril@gmail.com>
2024-09-27 13:19:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import MyBar from "./components/MyBar.vue";
+import MyCard from "./components/MyCard.vue";
+import items from "./attractions.json";
+</script>
+
+<template>
+  <header>
+    <MyBar />
+  </header>
+
+  <main>
+    <h1>Attractions</h1>
+    <div className="grid-container">
+      <MyCard v-for="item in items" v-bind="item" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  padding-left: 10px;
+}
+
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+}
+@media (min-width: 576px) {
+  .grid-container {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+@media (min-width: 992px) {
+  .grid-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+h1::first-letter {
+  color: rgb(112, 248, 141);
+  font-size: 50px;
+}
+
+h1:hover {
+  color: dodgerblue;
+}
+</style>
---
+++ src/components/MyCard.vue
@@ -0,0 +1,37 @@
+<template lang="">
+  <div class="card">
+    <img :src="coverimage" :alt="name" style="width: 100%" />
+    <div class="container">
+      <h4>
+        <b>{{ name }}</b>
+      </h4>
+      <p>
+        {{ detail }}
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default { props: ["id", "name", "coverimage", "detail"] };
+</script>
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  margin: 20px;
+  border-radius: 5%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+
+h4::first-line {
+  color: rgb(64, 116, 237);
+  font-size: 30px;
+}
+</style>
